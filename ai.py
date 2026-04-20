19f1d7d572221d5685e3d1eb812a514f569d360e
JLabandon <2917369609@qq.com>
2026-04-20 15:30:53 -0700

    finish ai.py

--- ai.py
+++ ai.py
@@ -19,7 +19,9 @@ class Node:
     # returns whether this is a terminal state (i.e., no children)
     def is_terminal(self):
         #TODO: complete this
-        pass
+        if not self.children:
+            return True
+        return False
 
 # AI agent. Determine the next move.
 class AI:
@@ -34,22 +36,90 @@ class AI:
 
     # TODO: build a game tree from the current node up to the given depth
     def build_tree(self, node = None, depth = 0):
-        pass
+        if node.player_type == MAX_PLAYER:
+            for direction in range(4):
+                self.simulator.set_state(*node.state)
+                if self.simulator.move(direction):
+                    new_state = self.simulator.current_state()
+                    child_node = Node(new_state, CHANCE_PLAYER)
+                    node.children.append((direction, child_node))
+                    if depth > 1:
+                        self.build_tree(child_node, depth - 1)
+        else:
+            tile_value = 2
+            for i in range(self.simulator.board_size):
+                for j in range(self.simulator.board_size):
+                    if node.state[0][i][j]:
+                        continue
+                    self.simulator.set_state(*node.state)
+                    self.simulator.tile_matrix[i][j] = tile_value
+                    new_state = self.simulator.current_state()
+                    child_node = Node(new_state, MAX_PLAYER)
+                    node.children.append((None, child_node))
+                    if depth > 1:
+                        self.build_tree(child_node, depth - 1)
 
     # TODO: expectimax calculation.
     # Return a (best direction, expectimax value) tuple if node is a MAX_PLAYER
     # Return a (None, expectimax value) tuple if node is a CHANCE_PLAYER
     def expectimax(self, node = None):
-        # TODO: delete this random choice but make sure the return type of the function is the same
-        return random.randint(0, 3), 0
+        if not node.children:
+            return None, node.state[1]
+        if node.player_type == MAX_PLAYER:
+            best_direction, best_value = None, float('-inf')
+            for direction, child in node.children:
+                _, value = self.expectimax(child)
+                if value > best_value:
+                    best_direction, best_value = direction, value
+            return best_direction, best_value
+        else:
+            total_value = 0
+            for _, child in node.children:
+                _, value = self.expectimax(child)
+                total_value += value
+            return None, total_value / len(node.children)
 
     # Return decision at the root
     def compute_decision(self):
         self.build_tree(self.root, self.search_depth)
         direction, _ = self.expectimax(self.root)
         return direction
+    
+    NEW_DEPTH = 5
+    RATIO = 100
 
+    def penalty_points(self, state):
+        penalty = 0
+        tile_matrix = state[0]
+        l = self.simulator.board_size
+        for i in range(l):
+            for j in range(l):
+                dx = min(i+1, l-i)
+                dy = min(j+1, l-j)
+                penalty += (dx+dy-1) * tile_matrix[i][j]
+
+        return penalty * self.RATIO
+
+    def expectimax_extension(self, node = None):
+        if not node.children:
+            return None, node.state[1]
+        if node.player_type == MAX_PLAYER:
+            best_direction, best_value = None, float('-inf')
+            for direction, child in node.children:
+                _, value = self.expectimax(child)
+                if value > best_value:
+                    best_direction, best_value = direction, value
+            return best_direction, best_value - self.penalty_points(node.state)
+        else:
+            total_value = 0
+            for _, child in node.children:
+                _, value = self.expectimax(child)
+                total_value += value
+            return None, total_value / len(node.children) - self.penalty_points(node.state)
+    
     # TODO (optional): the extension part
     def compute_decision_extension(self):
-        return random.randint(0, 3)
+        self.build_tree(self.root, self.NEW_DEPTH)
+        direction, _ = self.expectimax_extension(self.root)
+        return direction
 
